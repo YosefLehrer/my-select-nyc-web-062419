26ba6c34d6b764c742695ea0dcf02860af68bdd8
Yosef Lehrer <yolehrer@gmail.com>
2019-06-17 20:29:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
+=begin
 def my_select(collection)
- # your code here!
+  
 end
+=end
+
+def my_select(collection)
+  i = 0
+  empty_array =[]
+  while 
+  i < collection.length
+  empty_array << yield(collection[i])
+  i+=1
+  end
+  empty_array
+end
+
